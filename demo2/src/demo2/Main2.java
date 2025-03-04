857ef79608e144ffc6fe7a58aee7b6d68890bb8a
ASUS <ASUS@Sick-CS>
2025-03-04 13:25:34 +0530

    fork update

--- demo2/src/demo2/Main2.java
+++ demo2/src/demo2/Main2.java
@@ -1,7 +1,8 @@
 package demo2;
 
 public class Main2 {
-public static void main(String[] args) {
-	System.out.println("file 2.0");
-}
+	public static void main(String[] args) {
+		System.out.println("file 2.0");
+		System.out.println("file 3.0");
+	}
 }
